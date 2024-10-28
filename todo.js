b562527456f862476445ba8b7f8e5fe9b01272a5
Shruthi Kashaveni <141952842+KashaveniSruthi@users.noreply.github.com>
2024-10-28 17:42:28 +0530

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,140 +1,71 @@
-let todoItemsContainer = document.getElementById("todoItemsContainer");
-let addTodoButton = document.getElementById("addTodoButton");
-let saveTodoButton = document.getElementById("saveTodoButton");
+let tasks=[];
 
-function getTodoListFromLocalStorage() {
-  let stringifiedTodoList = localStorage.getItem("todoList");
-  let parsedTodoList = JSON.parse(stringifiedTodoList);
-  if (parsedTodoList === null) {
-    return [];
-  } else {
-    return parsedTodoList;
-  }
-}
+const addTask = ()=> {
+    const taskInput=document.getElementById("taskInput");
+    const text=taskInput.value.trim();
 
-let todoList = getTodoListFromLocalStorage();
-let todosCount = todoList.length;
-
-saveTodoButton.onclick = function() {
-  localStorage.setItem("todoList", JSON.stringify(todoList));
+    if(text){
+        tasks.push({text:text, completed: false });
+        taskInput.value = "";
+        updateTasksList();
+        updateStats();
+    }
 };
 
-function onAddTodo() {
-  let userInputElement = document.getElementById("todoUserInput");
-  let userInputValue = userInputElement.value;
-
-  if (userInputValue === "") {
-    alert("Enter Valid Text");
-    return;
-  }
-
-  todosCount = todosCount + 1;
-
-  let newTodo = {
-    text: userInputValue,
-    uniqueNo: todosCount,
-    isChecked: false
-  };
-  todoList.push(newTodo);
-  createAndAppendTodo(newTodo);
-  userInputElement.value = "";
-}
-
-addTodoButton.onclick = function() {
-  onAddTodo();
+const toggleTaskComplete=(index)=>{
+    tasks[index].completed=!tasks[index].completed;
+    updateTasksList();
+    updateStats();
 };
 
-function onTodoStatusChange(checkboxId, labelId, todoId) {
-  let checkboxElement = document.getElementById(checkboxId);
-  let labelElement = document.getElementById(labelId);
-  labelElement.classList.toggle("checked");
+const deleteTask= (index)=>{
+    tasks.splice(index,1);
+    updateTasksList();
+    updateStats();
+};
 
-  let todoObjectIndex = todoList.findIndex(function(eachTodo) {
-    let eachTodoId = "todo" + eachTodo.uniqueNo;
+const editTask=(index)=>{
+    const task=document.getElementById("taskInput");
+    taskInput.value=tasks[index].text;
 
-    if (eachTodoId === todoId) {
-      return true;
-    } else {
-      return false;
-    }
-  });
+    tasks.splice(index, 1);
+    updateTasksList();
+    updateStats();
+};
 
-  let todoObject = todoList[todoObjectIndex];
+const updateStats=()=>{
+    const completedTasks=tasks.filter((task)=>task.completed).length;
+    const totalTasks=tasks.length;
+    const progress=( completedTasks/totalTasks )*100;
+    const progressBar=document.getElementById("progress");
+    progressBar.style.width=`${progress}%`;
+    document.getElementById('numbers').innerText=`${completedTasks} / ${totalTasks}`
+};
 
-  if(todoObject.isChecked === true){
-    todoObject.isChecked = false;
-  } else {
-    todoObject.isChecked = true;
-  }
+const updateTasksList = () => {
+    const taskList=document.getElementById("task-list");
+    taskList.innerHTML = "";
 
-}
+    tasks.forEach((task, index) => {
+        const listItem=document.createElement("li");
 
-function onDeleteTodo(todoId) {
-  let todoElement = document.getElementById(todoId);
-  todoItemsContainer.removeChild(todoElement);
-
-  let deleteElementIndex = todoList.findIndex(function(eachTodo) {
-    let eachTodoId = "todo" + eachTodo.uniqueNo;
-    if (eachTodoId === todoId) {
-      return true;
-    } else {
-      return false;
-    }
-  });
-
-  todoList.splice(deleteElementIndex, 1);
-}
-
-function createAndAppendTodo(todo) {
-  let todoId = "todo" + todo.uniqueNo;
-  let checkboxId = "checkbox" + todo.uniqueNo;
-  let labelId = "label" + todo.uniqueNo;
-
-  let todoElement = document.createElement("li");
-  todoElement.classList.add("todo-item-container", "d-flex", "flex-row");
-  todoElement.id = todoId;
-  todoItemsContainer.appendChild(todoElement);
-
-  let inputElement = document.createElement("input");
-  inputElement.type = "checkbox";
-  inputElement.id = checkboxId;
-  inputElement.checked = todo.isChecked;
-
-  inputElement.onclick = function () {
-    onTodoStatusChange(checkboxId, labelId, todoId);
-  };
-
-  inputElement.classList.add("checkbox-input");
-  todoElement.appendChild(inputElement);
-
-  let labelContainer = document.createElement("div");
-  labelContainer.classList.add("label-container", "d-flex", "flex-row");
-  todoElement.appendChild(labelContainer);
-
-  let labelElement = document.createElement("label");
-  labelElement.setAttribute("for", checkboxId);
-  labelElement.id = labelId;
-  labelElement.classList.add("checkbox-label");
-  labelElement.textContent = todo.text;
-  if (todo.isChecked === true) {
-    labelElement.classList.add("checked");
-  }
-  labelContainer.appendChild(labelElement);
-
-  let deleteIconContainer = document.createElement("div");
-  deleteIconContainer.classList.add("delete-icon-container");
-  labelContainer.appendChild(deleteIconContainer);
-
-  let deleteIcon = document.createElement("i");
-  deleteIcon.classList.add("far", "fa-trash-alt", "delete-icon");
-
-  deleteIcon.onclick = function () {
-    onDeleteTodo(todoId);
-  };
-
-  deleteIconContainer.appendChild(deleteIcon);
-}
-
-for (let todo of todoList) {
-  createAndAppendTodo(todo);
-}
+        listItem.innerHTML =`
+        <div class="taskItem">
+            <div class="task ${task.completed ?"completed":""}">
+                <input type="checkbox" class="checkbox" ${task.completed ? "checked":""}/>
+                <p>${task.text}</p>
+            </div>
+            <div class="icons">
+                <img src="https://cdn-icons-png.flaticon.com/128/10308/10308644.png" onClick="editTask(${index})"/>
+                <img src="https://cdn-icons-png.flaticon.com/128/6861/6861362.png" onClick="deleteTask(${index})"/>
+            </div>
+        </div>
+        `;
+    listItem.addEventListener("change",() =>toggleTaskComplete(index));
+    taskList.append(listItem);
+    });
+};
+document.getElementById("newTask").addEventListener("click",function(e){
+    e.preventDefault();
+    addTask();
+});
